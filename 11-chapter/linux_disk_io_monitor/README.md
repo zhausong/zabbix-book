51a35ee3b03158e4bd5e5e52e44cc1e78543d704
itnihao <itnihao@qq.com>
2014-12-02 10:29:19 +0800

    add linux disk IO monitor

---
+++ 11-chapter/linux_disk_io_monitor/README.md
@@ -0,0 +1,9 @@
+#scripts and conf
+```
+#/etc/zabbix/scripts/check_harddisk.sh
+#/etc/zabbix/zabbix_agentd.conf.d/userparameter_harddisk.conf 
+chown zabbix.zabbix /etc/zabbix/scripts/check_harddisk.sh
+chmod 755           /etc/zabbix/scripts/check_harddisk.sh
+```
+#template
+import-inux_disk_io_template.xml
---
+++ 11-chapter/linux_disk_io_monitor/check_harddisk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# function:monitor redisstatus from zabbix
+# License: GPL
+# mail:admin@itnihao.com
+# version:1.0 date:2013-02-04
+
+diskname_discovery () {
+    HardDisk=($(grep '\b[a-z][a-z][a-z]\b'  /proc/diskstats|awk '{print $3}'))
+    [ "${HardDisk[0]}" == "" ] && exit
+    printf '{\n'
+    printf '\t"data":[\n'
+    for((i=0;i<${#HardDisk[@]};++i))
+    {
+        num=$(echo $((${#HardDisk[@]}-1)))
+        if [ "$i" != ${num} ];
+        then
+           printf "\t\t{ \n"
+           printf "\t\t\t\"{#DISKNAME}\":\"${HardDisk[$i]}\"},\n"
+        else
+           printf  "\t\t{ \n"
+           printf  "\t\t\t\"{#DISKNAME}\":\"${HardDisk[$num]}\"}]}\n"
+        fi
+    }
+}
+
+case "$1" in
+diskname_discovery)
+    diskname_discovery
+    ;;
+*)
+    echo "Usage: $0 {diskname_discovery}"
+    ;;
+esac
